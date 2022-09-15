8d6ac5d13351272b66d956f7d088001b94c56ce6
weijiahua <weijh@aixahc.com>
2022-09-15 18:00:11 +0800

    feat:dart变量常量声明

---
+++ demo1/demo2.dart
@@ -0,0 +1,28 @@
+void main() {
+  var str = 'hello dart';
+  var num = 123;
+  print(str);
+  print(num);
+
+  //字符串
+  String st = 'hello dark night';
+  print(st);
+
+  //数字
+  int n = 1224;
+  print(n);
+
+  //const常量
+  const PI = 3.1415926;
+  // PI= PI+1;//常量不可以修改
+  print(PI);
+
+  //final常量
+  final AS = 3.15;
+  // AS = AS + 1;
+  print(AS);
+
+  final time = new DateTime.now();
+  print(time);
+
+}
